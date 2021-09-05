7176d873fcae46170949d76d4211f957cfb4720c
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-09-05 17:25:13 +0200

    init

--- Apple_Frameworks_SwiftUI_MVVM/FrameworkGridView.swift
+++ Apple_Frameworks_SwiftUI_MVVM/FrameworkGridView.swift
@@ -7,15 +7,21 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FrameworkGridView: View {
+   
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            VStack{
+                Image("classkit")
+                
+                Text("CloudKit")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FrameworkGridView()
     }
 }
