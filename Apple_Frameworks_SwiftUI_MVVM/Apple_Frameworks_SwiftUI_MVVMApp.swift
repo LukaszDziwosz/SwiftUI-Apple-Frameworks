60d692b1dc489aeb43ec26120be536c6ab823262
LukaszDziwosz <35117633+LukaszDziwosz@users.noreply.github.com>
2021-09-05 16:38:31 +0200

    Initial Commit

---
+++ Apple_Frameworks_SwiftUI_MVVM/Apple_Frameworks_SwiftUI_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_Frameworks_SwiftUI_MVVMApp.swift
+//  Apple_Frameworks_SwiftUI_MVVM
+//
+//  Created by Lukasz Dziwosz on 05/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct Apple_Frameworks_SwiftUI_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
